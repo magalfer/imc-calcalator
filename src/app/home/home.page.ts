d28ec2d205ca265f8b50c9a6810c0270aaa37132
magalfer <matheus.ferreira@estudante.ufla.br>
2021-09-23 22:09:40 -0300

    add imc classification

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,21 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    var classification;
+
+    if(imc < 18.5){
+      classification = 'Magreza';
+    } else if( imc >= 18.5 &&  imc <= 24.9) {
+      classification = 'Normal';
+    } else if( imc > 25 && imc <29.9) {
+      classification = 'Sobrepeso';
+    } else if(imc > 30 && imc <39.9) {
+      classification = 'Obsidade';
+    } else {
+      classification = 'Obsidade Grave';
+    }
+
+    this.showMessage(`IMC = ${imc.toFixed(2)} - ${classification}`);
   }
 
   async showMessage(msg: string) {
